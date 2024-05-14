976a51fdb8ff1f5d421c9274959c82c5b7f023f7
margaridalvesmm <153611770+margaridalvesmm@users.noreply.github.com>
2024-05-14 21:20:51 +0100

    produtos do utilizador

--- controllers/user.js
+++ controllers/user.js
@@ -1,7 +1,8 @@
-const User = require('../models/user')
-const Location = require('../models/location')
-const { Sequelize } = require('sequelize')
-const bcrypt = require('bcrypt')
+const User = require('../models/user');
+const Location = require('../models/location');
+const { Sequelize } = require('sequelize');
+const bcrypt = require('bcrypt');
+const Product = require('../models/product');
 
 // exports.getAllUsers = async (req, res) => {
 //     try {
@@ -25,9 +26,9 @@ const bcrypt = require('bcrypt')
 
 exports.searchUsersByUsername = async (req, res) => {
     try {
-        const { username, page } = req.query
-        const limit = 20
-        const offset = (page - 1) * limit
+        const { username, page } = req.query;
+        const limit = 20;
+        const offset = (page - 1) * limit;
 
         const users = await User.findAll({
             where: {
@@ -38,14 +39,14 @@ exports.searchUsersByUsername = async (req, res) => {
             attributes: ['id', 'username', 'name'],
             limit,
             offset,
-        })
+        });
 
-        res.json(users)
+        res.json(users);
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'Failed to search users.' })
+        console.error(error);
+        res.status(500).json({ message: 'Failed to search users.' });
     }
-}
+};
 
 exports.getUser = async (req, res) => {
     const { id } = req.params;
@@ -61,11 +62,9 @@ exports.getUser = async (req, res) => {
 
 exports.registerUser = async (req, res) => {
     try {
-        const {
- username, name, email, gender, password, location 
-} = req.body
-        const saltRounds = 10
-        const passwordHash = await bcrypt.hash(password, saltRounds)
+        const { username, name, email, gender, password, location } = req.body;
+        const saltRounds = 10;
+        const passwordHash = await bcrypt.hash(password, saltRounds);
 
         const newUser = await User.create({
             username,
@@ -73,59 +72,59 @@ exports.registerUser = async (req, res) => {
             email,
             gender,
             password: passwordHash,
-        })
+        });
 
         if (location) {
-            const { locationName, address } = location
+            const { locationName, address } = location;
             const newLocation = await Location.create({
                 locationName,
                 address,
-            })
-            await newUser.addLocation(newLocation)
+            });
+            await newUser.addLocation(newLocation);
         }
 
-        res.status(201).json(newUser)
+        res.status(201).json(newUser);
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'Failed to register user.' })
+        console.error(error);
+        res.status(500).json({ message: 'Failed to register user.' });
     }
-}
+};
 
 exports.loginUser = async (req, res) => {
     try {
-        const { username, password } = req.body
+        const { username, password } = req.body;
 
-        const user = await User.findOne({ where: { username } })
+        const user = await User.findOne({ where: { username } });
         if (!user) {
-            throw new Error('User not found')
+            throw new Error('User not found');
         }
 
-        const passwordCheck = await bcrypt.compare(password, user.password)
+        const passwordCheck = await bcrypt.compare(password, user.password);
         if (!passwordCheck) {
-            throw new Error('Invalid password')
+            throw new Error('Invalid password');
         }
 
-        req.session.user = username
+        req.session.user = username;
 
         res.status(200).json({
             message: `${req.session.user} is now logged in.`,
-        })
+        });
     } catch (error) {
-        console.error(error)
-        res.status(401).json({ message: 'i he koe' })
+        console.error(error);
+        res.status(401).json({ message: 'i he koe' });
     }
-}
+};
 
 exports.deleteUser = async (req, res) => {
     try {
-        const loggedUser = req.session.user
+        const loggedUser = req.session.user;
 
-        const { username } = req.query
+        const { username } = req.query;
 
         if (loggedUser !== username) {
             return res
                 .status(403)
-                .json({ message: `${loggedUser} can't delete ${username}` })
+                .json({ message: `${loggedUser} can't delete ${username}` });
         }
 
         await User.destroy({
@@ -133,35 +132,33 @@ exports.deleteUser = async (req, res) => {
                 username: username,
             },
             attributes: ['username'],
-        })
+        });
 
-        res.status(200).json({ message: `${username} has been deleted` })
+        res.status(200).json({ message: `${username} has been deleted` });
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'Ko Iron Man ahau' })
+        console.error(error);
+        res.status(500).json({ message: 'Ko Iron Man ahau' });
     }
-}
+};
 
 exports.logoutUser = async (req, res) => {
     try {
-        req.session.destroy()
-        res.status(200).json({ message: 'Logged out successfully' })
+        req.session.destroy();
+        res.status(200).json({ message: 'Logged out successfully' });
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'Failed to logout' })
+        console.error(error);
+        res.status(500).json({ message: 'Failed to logout' });
     }
-}
+};
 
 exports.editUser = async (req, res) => {
     try {
-        const loggedUser = req.session.user
+        const loggedUser = req.session.user;
         if (!loggedUser) {
-            return res.status(404).json({ message: 'User not found' })
+            return res.status(404).json({ message: 'User not found' });
         }
 
-        const {
- username, name, email, gender, bio 
-} = req.body
+        const { username, name, email, gender, bio } = req.body;
 
         await User.update(
             {
@@ -174,41 +171,41 @@ exports.editUser = async (req, res) => {
             {
                 where: { username: loggedUser },
             }
-        )
+        );
 
-        res.status(201).json({ message: 'User updated' })
+        res.status(201).json({ message: 'User updated' });
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'Failed to edit user.' })
+        console.error(error);
+        res.status(500).json({ message: 'Failed to edit user.' });
     }
-}
+};
 
 exports.editPassword = async (req, res) => {
     try {
-        const loggedUser = req.session.user
+        const loggedUser = req.session.user;
         if (!loggedUser) {
-            return res.status(404).json({ message: 'User not found' })
+            return res.status(404).json({ message: 'User not found' });
         }
 
-        const { password } = req.body
+        const { password } = req.body;
 
         const existingUser = await User.findOne({
             where: { username: loggedUser },
-        })
+        });
 
         const passwordCheck = await bcrypt.compare(
             password,
             existingUser.password
-        )
+        );
         if (passwordCheck) {
             return res.status(403).json({
                 message:
                     'New password needs to be diferent from last password.',
-            })
+            });
         }
 
-        const saltRounds = 10
-        const passwordHash = await bcrypt.hash(password, saltRounds)
+        const saltRounds = 10;
+        const passwordHash = await bcrypt.hash(password, saltRounds);
 
         await User.update(
             {
@@ -217,57 +214,96 @@ exports.editPassword = async (req, res) => {
             {
                 where: { username: loggedUser },
             }
-        )
+        );
 
-        res.status(201).json({ message: 'Password updated' })
+        res.status(201).json({ message: 'Password updated' });
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'Failed to change password.' })
+        console.error(error);
+        res.status(500).json({ message: 'Failed to change password.' });
     }
-}
+};
 
 exports.editLocation = async (req, res) => {
     try {
-        const loggedUser = req.session.user
+        const loggedUser = req.session.user;
         if (!loggedUser) {
-            return res.status(404).json({ message: 'User not found' })
+            return res.status(404).json({ message: 'User not found' });
         }
 
-        const { locationId, name, address } = req.body
+        const { locationId, name, address } = req.body;
 
-        const user = await User.findOne({ where: { username: loggedUser } })
+        const user = await User.findOne({ where: { username: loggedUser } });
 
-        const userLocation = await Location.findByPk(locationId)
+        const userLocation = await Location.findByPk(locationId);
 
         if (!userLocation || userLocation.UserId !== user.id) {
             return res.status(404).json({
                 message: 'Location not found or does not belong to the user',
-            })
+            });
         }
 
-        await userLocation.update({ name, address })
+        await userLocation.update({ name, address });
 
-        res.status(200).json({ message: 'Location updated' })
+        res.status(200).json({ message: 'Location updated' });
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'Failed to edit location.' })
+        console.error(error);
+        res.status(500).json({ message: 'Failed to edit location.' });
     }
-}
+};
 
 exports.listUserLocations = async (req, res) => {
     try {
-        const loggedUser = req.session.user
+        const loggedUser = req.session.user;
         if (!loggedUser) {
-            return res.status(404).json({ message: 'User not found' })
+            return res.status(404).json({ message: 'User not found' });
         }
 
-        const user = await User.findOne({ where: { username: loggedUser } })
+        const user = await User.findOne({ where: { username: loggedUser } });
 
-        const userLocations = await user.getLocations()
+        const userLocations = await user.getLocations();
 
-        res.status(200).json(userLocations)
+        res.status(200).json(userLocations);
     } catch (error) {
-        console.error(error)
-        res.status(500).json({ message: 'Failed to list user locations.' })
+        console.error(error);
+        res.status(500).json({ message: 'Failed to list user locations.' });
     }
-}
+};
+
+exports.searchUsersByUsername = async (req, res) => {
+    try {
+        const { username, page } = req.query;
+        const limit = 20;
+        const offset = (page - 1) * limit;
+
+        const users = await User.findAll({
+            where: {
+                username: {
+                    [Sequelize.Op.like]: `%${username}%`,
+                },
+            },
+            attributes: ['id', 'username', 'name'],
+            include: [{ model: Product, attributes: ['id'], as: 'products' }],
+            limit,
+            offset,
+        });
+
+        res.json(users);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to search users.' });
+    }
+};
+
+exports.getUser = async (req, res) => {
+    const { id } = req.params;
+    console.log('id', id);
+    try {
+        const user = await User.findByPk(id, {
+            include: [{ model: Product, attributes: ['id'], as: 'products' }], // Incluindo os produtos do usuário
+        });
+        res.json(user);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Failed to fetch user.' });
+    }
+};
--- models/user.js
+++ models/user.js
@@ -1,33 +1,33 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
-
+const Product = require('./product');
 
 const User = sequelize.define('User', {
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  name: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  gender: {
-    type: DataTypes.STRING,
-  },
-  password: {
-    type: DataTypes.STRING,
-  },
-  bio: {
-    type: DataTypes.STRING,
-  },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    gender: {
+        type: DataTypes.STRING,
+    },
+    password: {
+        type: DataTypes.STRING,
+    },
+    bio: {
+        type: DataTypes.STRING,
+    },
 });
 
-
+User.hasMany(Product, { foreignKey: 'UserId', as: 'products' });
 
 module.exports = User;
