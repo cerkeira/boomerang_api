f905de88ce6ab4d0563c81ac2c38cd1451516afd
cerkeira <ricardocerqueira0@protonmail.com>
2024-05-26 09:16:46 +0100

    Automização da OpenAPI

--- routes/favorite.js
+++ routes/favorite.js
@@ -3,13 +3,109 @@ const router = express.Router();
 const favoriteController = require('../controllers/favorite');
 const { check } = require('express-validator');
 
+/**
+ * @swagger
+ * /favorite:
+ *   get:
+ *     tags:
+ *       - Favorites
+ *     summary: Ver produtos favoritos do utilizador logado
+ *     responses:
+ *       200:
+ *         description: List of favorite products
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Product'
+ *       401:
+ *         description: Utilizador não está logado
+ *       500:
+ *         description: Erro ao encontrar produtos favoritos
+ */
 router.get('/', favoriteController.getFavoriteProducts);
 
-// router.post('/add', favoriteController.addToFavorites);
-router.post('/add', [
-    check('productId').isInt().withMessage('Product ID must be an integer'),
-], favoriteController.addToFavorites);
+/**
+ * @swagger
+ * /favorite/add:
+ *   post:
+ *     tags:
+ *       - Favorites
+ *     summary: Adicionar produto aos favoritos
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - productId
+ *             properties:
+ *               productId:
+ *                 type: integer
+ *     responses:
+ *       201:
+ *         description: Produto adicionado aos favoritos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Input Inválido
+ *       401:
+ *         description: Utilizador não está logado
+ *       404:
+ *         description: Produto não encontrado
+ *       500:
+ *         description: Erro ao adicionar produto aos favoritos
+ */
+router.post(
+    '/add',
+    [check('productId').isInt().withMessage('Product ID must be an integer')],
+    favoriteController.addToFavorites
+);
 
+/**
+ * @swagger
+ * /favorite/remove:
+ *   post:
+ *     tags:
+ *       - Favorites
+ *     summary: Remover produto dos favoritos
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - productId
+ *             properties:
+ *               productId:
+ *                 type: integer
+ *     responses:
+ *       200:
+ *         description: Produto removido dos favoritos
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       400:
+ *         description: Input Inválido
+ *       401:
+ *         description: Utilizador não está logado
+ *       404:
+ *         description: Produto não está nos favoritos
+ *       500:
+ *         description: Erro ao remover produto dos favoritos
+ */
 router.post('/remove', favoriteController.removeFromFavorites);
 
 module.exports = router;
--- routes/location.js
+++ routes/location.js
@@ -1,14 +1,134 @@
 const express = require('express');
-
 const router = express.Router();
 const locationController = require('../controllers/location');
 
+/**
+ * @swagger
+ * /location:
+ *   put:
+ *     tags:
+ *       - Locations
+ *     summary: Editar morada existente
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - id
+ *               - address
+ *             properties:
+ *               id:
+ *                 type: integer
+ *               address:
+ *                 type: string
+ *               locationName:
+ *                 type: string
+ *               postalCode:
+ *                 type: string
+ *     responses:
+ *       200:
+ *         description: Morada atualizada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Location'
+ *       400:
+ *         description: Input inváldio
+ *       404:
+ *         description: Morada não encontrada
+ *       500:
+ *         description: Erro ao editar morada
+ */
 router.put('/', locationController.editLocation);
 
+/**
+ * @swagger
+ * /location:
+ *   get:
+ *     tags:
+ *       - Locations
+ *     summary: Mostrar moradas do utilizador
+ *     responses:
+ *       200:
+ *         description: Lista de moradas
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Location'
+ *       500:
+ *         description: Erro ao procurar moradas
+ */
 router.get('/', locationController.listUserLocations);
 
+/**
+ * @swagger
+ * /location:
+ *   post:
+ *     tags:
+ *       - Locations
+ *     summary: Adicionar morada
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - address
+ *             properties:
+ *               address:
+ *                 type: string
+ *               locationName:
+ *                 type: string
+ *               postalCode:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: Morada adicionado com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Location'
+ *       400:
+ *         description: Input inválido
+ *       500:
+ *         description: Erro ao adicionar morada
+ */
 router.post('/', locationController.addLocation);
 
+/**
+ * @swagger
+ * /location:
+ *   delete:
+ *     tags:
+ *       - Locations
+ *     summary: Apagar morada por ID
+ *     parameters:
+ *       - in: query
+ *         name: id
+ *         schema:
+ *           type: integer
+ *         required: true
+ *         description: ID da morada a apagar
+ *     responses:
+ *       200:
+ *         description: Morada apagada com sucesso
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *       404:
+ *         description: Morada não encontrada
+ *       500:
+ *         description: Erro ao apagar morada
+ */
 router.delete('/', locationController.deleteLocation);
 
 module.exports = router;
