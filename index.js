d11c838c2ccb23aac48b57c0ef284ab1905274ab
cerkeira <ricardocerqueira0@protonmail.com>
2024-05-27 17:58:15 +0100

    Cors origin

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.use(
             httpOnly: true,
         },
     }),
-    cors({ credentials: true })
+    cors({ origin: 'http://localhost:3000', credentials: true })
 );
 
 app.use('/user', userRoutes);
