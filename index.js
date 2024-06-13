90fa0d2578085a9e4597a09263a72567c466d42e
githubTiagoMarques <al.tiagosoares@gmail.com>
2024-06-13 20:35:16 +0100

    lint fix

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 
 sequelize
     .sync({
-        //force: true,
+        // force: true,
     })
     .then(() => {
         app.listen(PORT, () => {
