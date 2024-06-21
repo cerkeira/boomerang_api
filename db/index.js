3e42fbb4f30373b2685e861b25b7ad2dfea14030
cerkeira <ricardocerqueira0@protonmail.com>
2024-06-21 19:47:06 +0100

    update to vercel

--- db/index.js
+++ db/index.js
@@ -5,12 +5,12 @@ process.env.NODE_ENV = process.argv.slice(3);
 const config = require('config');
 
 const sequelize = new Sequelize(
-    config.database,
-    config.username,
-    config.password,
+    process.env.database || config.database,
+    process.env.username || config.username,
+    process.env.password || config.password,
     {
-        host: config.host,
-        dialect: config.dialect,
+        host: process.env.host || config.host,
+        dialect: process.env.dialect || config.dialect,
         // logging: false,
     }
 );
